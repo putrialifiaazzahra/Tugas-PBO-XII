2b50d45ebed4320ad0458261fe485e009b5f455d
putrialifiaazzahra <putrialifiaa412@gmail.com>
2021-08-06 16:22:36 +0700

    Update SB Admin 2

--- hapus.php
+++ hapus.php
@@ -10,10 +10,10 @@ if (isset($_GET['hapus'])) {
     $queryhapus = $Db->delete($idwarga);
     if ($queryhapus==TRUE) {
         echo "<script>alert('Data Berhasil Dihapus!');
-        document.location.href='index.php';</script>";
+        document.location.href='tambah-data.php';</script>";
     }else {
         echo "<script>alert('Data Gagal Terhapus!');
-        document.location.href='index.php';</script>";
+        document.location.href='tambah-data.php';</script>";
     }
     
 }
